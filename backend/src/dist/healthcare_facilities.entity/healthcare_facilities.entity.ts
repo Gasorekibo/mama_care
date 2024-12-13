51ce9dff95c8b4e12848b0f1f5a3ff0eafa88703
Gasorekibo <m.gasore@alustudent.com>
2024-12-13 10:19:51 +0200

    facility entity

--- backend/src/dist/healthcare_facilities.entity/healthcare_facilities.entity.ts
+++ backend/src/dist/healthcare_facilities.entity/healthcare_facilities.entity.ts
@@ -1,12 +1,18 @@
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
-import { Location } from "../location.entity/location.entity";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Location } from '../location.entity/location.entity';
 
 @Entity()
 export class HealthcareFacility {
   @PrimaryGeneratedColumn()
   id: string;
 
-  @Column()
+  @Column({ unique: true, nullable: false })
   name: string;
 
   @ManyToOne(() => Location, {
@@ -14,7 +20,7 @@ export class HealthcareFacility {
     cascade: true,
     nullable: false,
     onDelete: 'CASCADE',
-    onUpdate: 'CASCADE'
+    onUpdate: 'CASCADE',
   })
   @JoinColumn()
   location: Location;
@@ -22,15 +28,24 @@ export class HealthcareFacility {
   @Column('simple-array')
   servicesOffered: string[];
 
-  @Column()
+  @Column({ unique: true, nullable: false })
   contactNumber: string;
 
   @Column('boolean')
   hasEmergencyServices: boolean;
+
+  @Column({ unique: true, nullable: false })
+  email: string;
 
   @Column('time')
   openingTime: string;
 
   @Column('time')
   closingTime: string;
+  @Column({
+    nullable: true,
+    default:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8MOvvaxkrLPSehZzvLBfMJM2Xk6UL1AVL8w&s',
+  })
+  profilePicture: string;
 }
