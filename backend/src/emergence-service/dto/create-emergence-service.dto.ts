33be66e42fdd00f8c0fefe754f41d5e3accd475e
Gasorekibo <m.gasore@alustudent.com>
2024-12-13 15:30:59 +0200

    feat: create dto

---
+++ backend/src/emergence-service/dto/create-emergence-service.dto.ts
@@ -0,0 +1,19 @@
+import { IsBoolean, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateEmergenceServiceDto {
+  @IsNotEmpty()
+  location: {
+    latitude: number;
+    longitude: number;
+  };
+
+  @IsNotEmpty()
+  @IsString()
+  emergencyType: string;
+
+  @IsBoolean()
+  isResolved: boolean;
+  @IsNotEmpty()
+  @IsNumber()
+  assignedFacilityId: number;
+}
