15167ac756fea1bacdf0ca125bf63efb9782576a
Gasorekibo <m.gasore@alustudent.com>
2024-12-15 09:44:22 +0200

    feat: user module

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,17 +1,11 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/dist/user/user.entity';
-import { HealthCheckupsService } from 'src/health-checkups/health-checkups.service';
-import { HealthCheckup } from 'src/dist/health_checkups.entity/health_checkups.entity';
-import { HealthCheckupsModule } from 'src/health-checkups/health-checkups.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([User]),
-    forwardRef(() => HealthCheckupsModule),
-  ],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
