2d5dddecb5c06889fdbb3f106b2599a5ecf2eba2
Gasorekibo <m.gasore@alustudent.com>
2024-12-17 14:12:48 +0200

    feat: add nearest hospital in facility module

--- backend/src/health-facility/health-facility.module.ts
+++ backend/src/health-facility/health-facility.module.ts
@@ -4,11 +4,12 @@ import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 import { HealthcareFacility } from 'src/dist/healthcare_facilities.entity/healthcare_facilities.entity';
 import { HealthFacilityController } from './health-facility.controller';
 import { HealthFacilityService } from './health-facility.service';
+import { NearbyHospitalService } from './nearbyHospitalService';
 
 @Module({
   imports: [TypeOrmModule.forFeature([HealthcareFacility])],
   controllers: [HealthFacilityController],
-  providers: [HealthFacilityService, CloudinaryService],
+  providers: [HealthFacilityService, CloudinaryService, NearbyHospitalService],
   exports: [HealthFacilityService],
 })
 export class HealthFacilityModule {}
