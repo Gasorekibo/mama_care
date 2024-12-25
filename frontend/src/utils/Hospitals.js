c60ac7f34936f746de4003548a3845f9e759cc88
Gasorekibo <m.gasore@alustudent.com>
2024-12-25 14:53:11 +0200

    feat: hospital file

---
+++ frontend/src/utils/Hospitals.js
@@ -0,0 +1,41 @@
+export const hospitals = [
+    {
+        hospitalName: "Hospital 1",
+        imageSource: "https://images.unsplash.com/photo-1557683316-973673baf926",
+        description: "Hospital 1 description",
+        owner: "Owner 1",
+        time: "1 hour ago",
+        ownerProfile: "https://images.unsplash.com/photo-1557683316-973673baf926"
+    },
+    {
+        hospitalName: "Hospital 2",
+        imageSource: "https://images.unsplash.com/photo-1557683316-973673baf926",
+        description: "Hospital 2 description",
+        owner: "Owner 2",
+        time: "2 hours ago",
+        ownerProfile: "https://images.unsplash.com/photo-1557683316-973673baf926"
+    },
+    {
+        hospitalName: "Hospital 3",
+        imageSource: "https://images.unsplash.com/photo-1557683316-973673baf926",
+        description: "Hospital 3 description",
+        owner: "Owner 3",
+        time: "3 hours ago",
+        ownerProfile: "https://images.unsplash.com/photo-1557683316-973673baf926"
+    }, 
+    {
+        hospitalName: "Hospital 4",
+        imageSource: "https://images.unsplash.com/photo-1557683316-973673baf926",
+        description: "Hospital 4 description",
+        owner: "Owner 4",
+        time: "4 hours ago",
+        ownerProfile: "https://images.unsplash.com/photo-1557683316-973673baf926"
+    }, {
+        hospitalName: "Hospital 5",
+        imageSource: "https://images.unsplash.com/photo-1557683316-973673baf926",
+        description: "Hospital 5 description",
+        owner: "Owner 5",
+        time: "5 hours ago",
+        ownerProfile: "https://images.unsplash.com/photo-1557683316-973673baf926"
+    }
+]
