b56e71b553269f42abddc21486d0fed1b4db7ca0
Gasorekibo <m.gasore@alustudent.com>
2024-12-25 14:52:49 +0200

    feat: navigation links

---
+++ frontend/src/components/Navigation.jsx
@@ -0,0 +1,99 @@
+import { Avatar, Dropdown, Navbar } from "flowbite-react";
+import { BiBookContent } from "react-icons/bi";
+import { CiLogin } from "react-icons/ci";
+import { GrSchedulePlay } from "react-icons/gr";
+import { MdAddHomeWork } from "react-icons/md";
+import { TbEmergencyBed } from "react-icons/tb";
+
+function Navigation() {
+  return (
+    <>
+      <Navbar
+        fluid
+        rounded
+        className="px-4 text-primaryText bg-slate-100 shadow-md"
+      >
+        <Navbar.Brand href="/">
+          <img
+            src="logo.png"
+            className="mr-3 h-6 sm:h-9"
+            alt="Mama care logo"
+          />
+        </Navbar.Brand>
+        <div className="flex md:order-2 z-50">
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar
+                alt="User settings"
+                img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                rounded
+              />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">Bonnie Green</span>
+              <span className="block truncate text-sm font-medium">
+                name@flowbite.com
+              </span>
+            </Dropdown.Header>
+            <Dropdown.Item>Dashboard</Dropdown.Item>
+            <Dropdown.Item>Settings</Dropdown.Item>
+            <Dropdown.Item>Earnings</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+          <Navbar.Toggle />
+        </div>
+        <Navbar.Collapse className="lg:ml-72">
+          {NAVIGATION_LINKS.map((link) => (
+            <Navbar.Link
+              key={link.name}
+              href={link.url}
+              className="flex items-center font-normal  hover:text-primary hover:bg-secondary rounded-md "
+            >
+              {link.icon}
+              <span className="ml-1">{link.name}</span>
+            </Navbar.Link>
+          ))}
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+}
+
+const NAVIGATION_LINKS = [
+  {
+    name: "Home",
+    url: "/",
+    icon: <MdAddHomeWork />,
+  },
+  {
+    name: "Pregnancy Education",
+    url: "/pregnancy-education",
+    icon: <BiBookContent />,
+  },
+  {
+    name: "Appointment",
+    url: "/appointment",
+    icon: <GrSchedulePlay />,
+  },
+  {
+    name: "Emergence",
+    url: "/emergence",
+    icon: <TbEmergencyBed />,
+  },
+  {
+    name: "Register",
+    url: "/register",
+    icon: <CiLogin />,
+  },
+  {
+    name: "Login",
+    url: "/login",
+    icon: <CiLogin />,
+  },
+];
+
+export default Navigation;
