21be6b6590de564319b90d670e3bd51c9b20c92a
Gasorekibo <m.gasore@alustudent.com>
2025-01-07 09:17:35 +0200

    feat: add a toast

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./redux/store/store.js";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+        <Toaster />
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
