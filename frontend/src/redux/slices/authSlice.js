402dda00526760426eaf22afb602aab05afc272b
Gasorekibo <m.gasore@alustudent.com>
2025-01-07 09:18:23 +0200

    feat: redux auth slice

---
+++ frontend/src/redux/slices/authSlice.js
@@ -0,0 +1,91 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { baseUrl } from "../../utils/baseUrl";
+
+export const registerAction = createAsyncThunk(
+  "user/register",
+  async (userData, { rejectWithValue }) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const response = await axios.post(
+        `${baseUrl}/auth/register`,
+        userData,
+        config
+      );
+      return response.data;
+    } catch (error) {
+      if (!error.response) {
+        throw error;
+      } else {
+        return rejectWithValue(error.response.data.message);
+      }
+    }
+  }
+);
+
+export const loginAction = createAsyncThunk(
+  "user/login",
+  async (userData, { rejectWithValue }) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const response = await axios.post(
+        `${baseUrl}/auth/login`,
+        userData,
+        config
+      );
+      return response.data;
+    } catch (error) {
+        console.log(error)
+      if (!error.response) {
+        throw error;
+      } else {
+        return rejectWithValue(error.response.data.message);
+      }
+    }
+  }
+);
+
+const userFromLocalStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    auth: userFromLocalStorage,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(registerAction.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(registerAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.auth = action.payload;
+    });
+    builder.addCase(registerAction.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    });
+    builder.addCase(loginAction.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(loginAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.auth = action.payload;
+      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+    });
+    builder.addCase(loginAction.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    });
+  },
+});
+
+export default userSlice.reducer;
