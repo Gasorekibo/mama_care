e3dbd919f7537a053c1d5e8faae921098f3c6fc3
Gasorekibo <m.gasore@alustudent.com>
2025-01-07 15:07:30 +0200

    feat: show based on user login status

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -5,9 +5,16 @@ import { GrSchedulePlay } from "react-icons/gr";
 import { MdAddHomeWork } from "react-icons/md";
 import { TbEmergencyBed } from "react-icons/tb";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Navigation() {
   const navigate = useNavigate();
+  let loginUser;
+  const { auth } = useSelector((state) => state?.auth);
+  if (auth) {
+    loginUser = auth?.user;
+  }
+
   function handleNavigation(link) {
     navigate(link);
   }
@@ -25,40 +32,45 @@ function Navigation() {
             alt="Mama care logo"
           />
         </Navbar.Brand>
-        <div className="flex md:order-2 z-50">
-          <Dropdown
-            arrowIcon={false}
-            inline
-            label={
-              <Avatar
-                alt="User settings"
-                img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                rounded
-              />
-            }
-          >
-            <Dropdown.Header>
-              <span className="block text-sm">Bonnie Green</span>
-              <span className="block truncate text-sm font-medium">
-                name@flowbite.com
-              </span>
-            </Dropdown.Header>
-            <Dropdown.Item onClick={() => handleNavigation("/profile/1")}>
-              Dashboard
-            </Dropdown.Item>
-            <Dropdown.Item onClick={() => handleNavigation("/profile/1")}>
-              Settings
-            </Dropdown.Item>
-            <Dropdown.Item onClick={() => handleNavigation("/profile/1")}>
-              Earnings
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item onClick={() => handleNavigation("/profile/1")}>
-              Sign out
-            </Dropdown.Item>
-          </Dropdown>
-          <Navbar.Toggle />
-        </div>
+        {loginUser && (
+          <div className="flex md:order-2 z-50">
+            <Dropdown
+              arrowIcon={false}
+              inline
+              label={
+                <Avatar
+                  alt="User settings"
+                  img={loginUser?.profilePicture}
+                  rounded
+                />
+              }
+            >
+              <Dropdown.Header>
+                <span className="block text-sm">{loginUser?.full_name}</span>
+                <span className="block truncate text-sm font-medium">
+                  {loginUser?.email}
+                </span>
+              </Dropdown.Header>
+              <Dropdown.Item
+                onClick={() => handleNavigation(`/profile/${loginUser?.id}`)}
+              >
+                Dashboard
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => handleNavigation("/profile/1")}>
+                Settings
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => handleNavigation("/profile/1")}>
+                Earnings
+              </Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item onClick={() => handleNavigation("/profile/1")}>
+                Sign out
+              </Dropdown.Item>
+            </Dropdown>
+            <Navbar.Toggle />
+          </div>
+        )}
+
         <Navbar.Collapse className="lg:ml-72">
           {NAVIGATION_LINKS.map((link) => (
             <Navbar.Link
