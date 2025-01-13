00fd6238ec8eec11b53d646b8d3935c78f3f68b4
Gasorekibo <m.gasore@alustudent.com>
2025-01-13 19:53:15 +0200

    feat: add user reducer in a store

--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authhReducer from "../slices/authSlice";
+import usersReducer from "../slices/userSlice";
 const store = configureStore({
   reducer: {
     auth: authhReducer,
+    users: usersReducer,
   },
 });
 
