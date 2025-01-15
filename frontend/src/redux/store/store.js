8a1ad59bf01380df181fae1477ac0af7c00e926b
Gasorekibo <m.gasore@alustudent.com>
2025-01-15 16:12:14 +0200

    feat: add education reducer in store

--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authhReducer from "../slices/authSlice";
 import usersReducer from "../slices/userSlice";
+import educationReducer from "../slices/educationSlice";
 const store = configureStore({
   reducer: {
     auth: authhReducer,
     users: usersReducer,
+    education: educationReducer,
   },
 });
 
