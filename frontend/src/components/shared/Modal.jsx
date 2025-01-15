1080624dd216d394df255e8e39da7cade315f9fe
Gasorekibo <m.gasore@alustudent.com>
2025-01-15 11:51:54 +0200

    feat: add new education modal

--- frontend/src/components/shared/Modal.jsx
+++ frontend/src/components/shared/Modal.jsx
@@ -1,31 +1,63 @@
-export default function Modal() {
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button, Label, Textarea, TextInput } from "flowbite-react";
+import PropTypes from "prop-types";
+import { Controller, useForm } from "react-hook-form";
+import { HiMail } from "react-icons/hi";
+import { FaLink } from "react-icons/fa";
+import { RiSkull2Fill } from "react-icons/ri";
+import { CgSelectR } from "react-icons/cg";
+import { MdOutlineTextSnippet } from "react-icons/md";
+import * as yup from "yup";
+
+export default function Modal({ modelId, isModalOpen, closeModal }) {
+  const schema = yup.object({
+    title: yup.string().required("Title is required"),
+    content: yup.string().required("Content is required"),
+    type: yup.string().required("Type is required"),
+    videoUrl: yup.string().required("Video URL is required"),
+    recommendedForRiskLevel: yup
+      .string()
+      .required("Recommended for risk level is required"),
+  });
+  const {
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      title: "",
+      content: "",
+      type: "",
+      videoUrl: "",
+      recommendedForRiskLevel: "",
+    },
+  });
+  const onSubmit = async (data) => {
+    console.log(data);
+  };
   return (
     <div>
-      <button
-        data-modal-target="crud-modal"
-        data-modal-toggle="crud-modal"
-        className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        type="button"
-      >
-        Toggle modal
-      </button>
-
       <div
-        id="crud-modal"
-        tabIndex={-1}
-        aria-hidden="true"
-        className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+        id={modelId}
+        tabIndex="-1"
+        aria-hidden={!isModalOpen}
+        className={`${
+          isModalOpen ? "flex" : "hidden"
+        } overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}
       >
-        <div className="relative p-4 w-full max-w-md max-h-full">
+        <div className="relative p-4 max-w-2xl w-full lg:max-w-4xl max-h-full">
           <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                Create New Product
+              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                Add New Education Content
               </h3>
               <button
                 type="button"
-                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                data-modal-toggle="crud-modal"
+                className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                data-modal-hide={modelId}
+                onClick={closeModal}
               >
                 <svg
                   className="w-3 h-3"
@@ -46,95 +78,149 @@ export default function Modal() {
               </button>
             </div>
 
-            <form className="p-4 md:p-5">
-              <div className="grid gap-4 mb-4 grid-cols-2">
-                <div className="col-span-2">
-                  <label
-                    htmlFor="name"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Name
-                  </label>
-                  <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                    placeholder="Type product name"
-                    required=""
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 px-10">
+              <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="title" value="Title" />
+                  </div>
+                  <Controller
+                    name="title"
+                    control={control}
+                    render={({ field }) => (
+                      <TextInput
+                        {...field}
+                        id="title"
+                        type="title"
+                        icon={HiMail}
+                        placeholder="Nutrition for Perinatal"
+                        color={errors.title?.message ? "failure" : "gray"}
+                        helperText={errors.title?.message}
+                      />
+                    )}
                   />
                 </div>
-                <div className="col-span-2 sm:col-span-1">
-                  <label
-                    htmlFor="price"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Price
-                  </label>
-                  <input
-                    type="number"
-                    name="price"
-                    id="price"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                    placeholder="$2999"
-                    required=""
+
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="content" value="Content" />
+                  </div>
+                  <Controller
+                    name="content"
+                    control={control}
+                    render={({ field }) => (
+                      <Textarea
+                        id="content"
+                        type="content"
+                        icon={MdOutlineTextSnippet}
+                        placeholder="Lorem ipsum dolor sit amet."
+                        color={errors.content?.message ? "failure" : "gray"}
+                        helperText={errors.content?.message}
+                        className="focus:border-black"
+                        {...field}
+                      />
+                    )}
                   />
                 </div>
-                <div className="col-span-2 sm:col-span-1">
-                  <label
-                    htmlFor="category"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Category
-                  </label>
-                  <select
-                    id="category"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                  >
-                    <option selected="">Select category</option>
-                    <option value="TV">TV/Monitors</option>
-                    <option value="PC">PC</option>
-                    <option value="GA">Gaming/Console</option>
-                    <option value="PH">Phones</option>
-                  </select>
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="type" value="Type" />
+                  </div>
+                  <Controller
+                    name="type"
+                    control={control}
+                    render={({ field }) => (
+                      <TextInput
+                        id="type"
+                        type="type"
+                        icon={CgSelectR}
+                        placeholder="Nutrition"
+                        color={errors.type?.message ? "failure" : "gray"}
+                        helperText={errors.type?.message}
+                        {...field}
+                      />
+                    )}
+                  />
                 </div>
-                <div className="col-span-2">
-                  <label
-                    htmlFor="description"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Product Description
-                  </label>
-                  <textarea
-                    id="description"
-                    rows="4"
-                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Write product description here"
-                  ></textarea>
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="videoUrl" value="Video Url" />
+                  </div>
+                  <Controller
+                    name="videoUrl"
+                    control={control}
+                    render={({ field }) => (
+                      <TextInput
+                        id="videoUrl"
+                        type="videoUrl"
+                        icon={FaLink}
+                        placeholder="https://www.youtube.com/watch?v=n04NPtZI4QQ"
+                        color={errors.videoUrl?.message ? "failure" : "gray"}
+                        helperText={errors.videoUrl?.message}
+                        {...field}
+                      />
+                    )}
+                  />
+                </div>
+                <div>
+                  <div className="mb-2 block">
+                    <Label
+                      htmlFor="recommendedForRiskLevel"
+                      value="Risk Level"
+                    />
+                  </div>
+                  <Controller
+                    name="recommendedForRiskLevel"
+                    control={control}
+                    render={({ field }) => (
+                      <TextInput
+                        id="recommendedForRiskLevel"
+                        type="recommendedForRiskLevel"
+                        icon={RiSkull2Fill}
+                        placeholder="MEDIUM"
+                        color={
+                          errors.recommendedForRiskLevel?.message
+                            ? "failure"
+                            : "gray"
+                        }
+                        helperText={errors.recommendedForRiskLevel?.message}
+                        {...field}
+                      />
+                    )}
+                  />
                 </div>
               </div>
-              <button
-                type="submit"
-                className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                <svg
-                  className="me-1 -ms-1 w-5 h-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
+
+              <div className="flex justify-end space-x-4 py-4">
+                <Button
+                  type="button"
+                  color=" orange"
+                  onClick={() => reset()}
+                  className="bg-orange-400  hover:bg-orange-300 text-white"
                 >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                Add new product
-              </button>
+                  Clear
+                </Button>
+                <Button type="submit" color="blue">
+                  Add New
+                </Button>
+              </div>
             </form>
           </div>
         </div>
       </div>
+
+      {isModalOpen && (
+        <div
+          className="bg-gray-900 bg-opacity-50 fixed inset-0 z-40"
+          onClick={closeModal}
+        ></div>
+      )}
     </div>
   );
 }
+
+Modal.propTypes = {
+  modelId: PropTypes.string.isRequired,
+  isModalOpen: PropTypes.bool.isRequired,
+  closeModal: PropTypes.func.isRequired,
+};
