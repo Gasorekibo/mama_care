209b969d5eee6be8456d74e7f2c5bad675fe71a1
Gasorekibo <m.gasore@alustudent.com>
2025-01-16 10:09:28 +0200

    refactor: all users in the platform

--- frontend/src/components/Dashboard/DashboardStatsGrid.jsx
+++ frontend/src/components/Dashboard/DashboardStatsGrid.jsx
@@ -1,33 +1,35 @@
-import { IoBagHandle, IoPieChart, IoPeople, IoCart } from "react-icons/io5";
-
+import { IoPeople } from "react-icons/io5";
+import { FaCalendarAlt } from "react-icons/fa";
+import { LiaHospitalSolid } from "react-icons/lia";
+import { IoIosWoman } from "react-icons/io";
 export default function DashboardStatsGrid() {
   return (
     <div className="flex gap-4">
       <BoxWrapper>
         <div className="rounded-full h-12 w-12 flex items-center justify-center bg-sky-500">
-          <IoBagHandle className="text-2xl text-white" />
+          <IoPeople className="text-2xl text-white" />
         </div>
         <div className="pl-4">
-          <span className="text-sm text-gray-500 font-light">Total Sales</span>
+          <span className="text-sm text-gray-500 font-light">Total Users</span>
           <div className="flex items-center">
             <strong className="text-xl text-gray-700 font-semibold">
-              $54232
+              54232
             </strong>
-            <span className="text-sm text-green-500 pl-2">+343</span>
+            <span className="text-sm text-blue-500 pl-2">+343</span>
           </div>
         </div>
       </BoxWrapper>
       <BoxWrapper>
         <div className="rounded-full h-12 w-12 flex items-center justify-center bg-orange-600">
-          <IoPieChart className="text-2xl text-white" />
+          <FaCalendarAlt className="text-2xl text-white" />
         </div>
         <div className="pl-4">
           <span className="text-sm text-gray-500 font-light">
-            Total Expenses
+            Total Appointments
           </span>
           <div className="flex items-center">
             <strong className="text-xl text-gray-700 font-semibold">
-              $3423
+              3423
             </strong>
             <span className="text-sm text-green-500 pl-2">-343</span>
           </div>
@@ -35,11 +37,11 @@ export default function DashboardStatsGrid() {
       </BoxWrapper>
       <BoxWrapper>
         <div className="rounded-full h-12 w-12 flex items-center justify-center bg-yellow-400">
-          <IoPeople className="text-2xl text-white" />
+          <LiaHospitalSolid className="text-2xl text-white" />
         </div>
         <div className="pl-4">
           <span className="text-sm text-gray-500 font-light">
-            Total Customers
+            Total Hospitals
           </span>
           <div className="flex items-center">
             <strong className="text-xl text-gray-700 font-semibold">
@@ -51,10 +53,10 @@ export default function DashboardStatsGrid() {
       </BoxWrapper>
       <BoxWrapper>
         <div className="rounded-full h-12 w-12 flex items-center justify-center bg-green-600">
-          <IoCart className="text-2xl text-white" />
+          <IoIosWoman className="text-2xl text-white" />
         </div>
         <div className="pl-4">
-          <span className="text-sm text-gray-500 font-light">Total Orders</span>
+          <span className="text-sm text-gray-500 font-light">Total Women</span>
           <div className="flex items-center">
             <strong className="text-xl text-gray-700 font-semibold">
               16432
