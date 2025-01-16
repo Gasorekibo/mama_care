68e07211bb3769871e8a62922109155aa3fd8a9e
Gasorekibo <m.gasore@alustudent.com>
2025-01-16 10:08:31 +0200

    feat: education slice

--- frontend/src/redux/slices/educationSlice.js
+++ frontend/src/redux/slices/educationSlice.js
@@ -26,6 +26,34 @@ export const getAllEducationAction = createAsyncThunk(
   }
 );
 
+export const addEducationAction = createAsyncThunk(
+  "education/add",
+  async (education, { rejectWithValue, getState }) => {
+    const user = getState()?.auth?.auth;
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user?.access_token}`,
+      },
+    };
+    try {
+      const response = await axios.post(
+        `${baseUrl}/education-content`,
+        education,
+        config
+      );
+      return response.data;
+    } catch (error) {
+      if (!error.response) {
+        throw error;
+      } else {
+        return rejectWithValue(error.response.data.message);
+      }
+    }
+  }
+);
+
 const educationSlice = createSlice({
   name: "education",
   initialState: {
@@ -45,8 +73,20 @@ const educationSlice = createSlice({
       .addCase(getAllEducationAction.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
+      })
+      .addCase(addEducationAction.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(addEducationAction.fulfilled, (state, action) => {
+        state.loading = false;
+        state.education.push(action.payload);
+      })
+      .addCase(addEducationAction.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
       });
   },
 });
 
 export default educationSlice.reducer;
+
