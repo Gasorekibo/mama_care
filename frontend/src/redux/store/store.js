1ce9d0a0f406bcdfc3473680914578cb7c10fc67
Gasorekibo <m.gasore@alustudent.com>
2025-01-17 11:56:44 +0200

    feat: update the store

--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authhReducer from "../slices/authSlice";
 import usersReducer from "../slices/userSlice";
 import educationReducer from "../slices/educationSlice";
+import hospitalReducer from "../slices/hospitalSlice";
 const store = configureStore({
   reducer: {
     auth: authhReducer,
     users: usersReducer,
     education: educationReducer,
+    hospitals: hospitalReducer,
   },
 });
 
